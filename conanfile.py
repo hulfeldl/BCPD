d4847c2976639a7ed0305da58e03ed3305ae8493
hulfeldl <lorenz.hulfeld@gmail.com>
2026-08-13 19:18:07 +0200

    Integrate KiSync-cmake as submodule for C++23 and optimization support

--- conanfile.py
+++ conanfile.py
@@ -1,11 +1,22 @@
 from conan import ConanFile
-from conan.tools.cmake import cmake_layout
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
 
 class BCPDConan(ConanFile):
-    name = "BCPD"
-    version = "0.1.0"
+    name = "bcpd"
+    version = "1.0"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps", "CMakeToolchain"
-    requires = ["fmt/11.0.2", "spdlog/1.14.1"]
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    def requirements(self):
+        # Add BCPD-specific dependencies here
+        # self.requires("fmt/10.1.1")
+        # self.requires("spdlog/1.13.0")
+        pass
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
     def layout(self):
         cmake_layout(self)
